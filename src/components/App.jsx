e89f7d008f166323a5cd98715b254b1b5326fb11
alexveryk <veryk.av@gmail.com>
2023-03-14 00:21:05 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import { About } from './pages/About';
+import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
+import { ProductDetails } from './pages/ProductDetails';
+import { Products } from './pages/Products';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        {/* Компонент <NavLink> відрізняється тільки тим, що може мати додаткові стилі, якщо поточний 
+        URL збігається зі значенням пропcа to. За замовчуванням елементу активного посилання додається 
+        клас active. Це можна використовувати для її стилізації. */}
+
+        <StyledLink to="/" end>
+          Home
+        </StyledLink>
+        <StyledLink to="/about">About</StyledLink>
+        <StyledLink to="/products">Products</StyledLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="products/:productId" element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
