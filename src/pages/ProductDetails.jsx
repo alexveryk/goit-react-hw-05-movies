65f5a94198b14435b67536a1b67b57462ab1596e
alexveryk <veryk.av@gmail.com>
2023-03-16 00:32:16 +0200

    Delete ProductDetails.jsx

--- src/pages/ProductDetails.jsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from 'react-router-dom';
-
-export const ProductDetails = () => {
-  const { productId } = useParams();
-  console.log(productId);
-  return <div>Now showing product with id - {productId}</div>;
-};
