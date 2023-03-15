6e90eea3265cb654dd45356e9b020475b2592baa
alexveryk <veryk.av@gmail.com>
2023-03-16 00:31:52 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-
-import { About } from '../pages/About';
-import { Home } from '../pages/Home';
-import { NotFound } from '../pages/NotFound';
-import { ProductDetails } from '../pages/ProductDetails';
-import { Products } from '../pages/Products';
-import { Mission } from './Mission';
-import { Reviews } from './Reviews';
-import { SharedLayout } from './SharedLayout';
-import { Team } from './Team';
+import { Home } from 'pages/Home';
+import { Movies } from 'pages/Movies';
+import { Routes, Route, NavLink } from 'react-router-dom';
 
 export const App = () => {
   return (
     <div>
+      <nav>
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />}>
-            <Route path="mission" element={<Mission />} />
-            <Route path="team" element={<Team />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-          <Route path="products" element={<Products />} />
-          <Route path="products/:productId" element={<ProductDetails />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
+        <Route path="*" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
       </Routes>
     </div>
   );
