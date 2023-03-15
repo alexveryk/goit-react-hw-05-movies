b135025839d2e1c6f8af14bf67d40dd5213b107f
alexveryk <veryk.av@gmail.com>
2023-03-16 00:32:09 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,53 @@
+import { getTrending } from 'components/API/api';
+import { useState, useEffect } from 'react';
+
 export const Home = () => {
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [totalHits, setTotalHits] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+
+        const response = await getTrending(page);
+
+        if (!response.data.total_results) {
+          console.log('Not Movies found! :(');
+        }
+
+        setMovies(response.data.results);
+        setTotalHits(response.data.total_results);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [page]);
+
   return (
     <div>
-      <h2>Home</h2>{' '}
+      <h1>Trending today</h1>
+      {loading && <p>Loading...</p>}
+      <ul>
+        {movies.map(movie => {
+          console.log(movie);
+          return (
+            <li key={movie.id}>
+              <img
+                // src={`https://image.tmdb.org/t/p/w200/${movie.backdrop_path}`}
+                alt={movie.title}
+              />
+              {movie.title}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
