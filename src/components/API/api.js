4a89a1a50e170aef62f8383ae02940cd56fe4a9e
alexveryk <veryk.av@gmail.com>
2023-03-20 00:29:22 +0200

    Update api.js

--- src/components/API/api.js
+++ src/components/API/api.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'cc9b731996eb433c4f02d82e82c7e11c';
+const page = 1;
 
-export const getTrending = async page => {
+export const getTrending = async () => {
   const response = await axios.get(
     `${BASE_URL}trending/movie/day?api_key=${API_KEY}&page=${page}`
   );
