742f9b7663c6396ba14fc212da9fd0279e685e3b
alexveryk <veryk.av@gmail.com>
2023-03-20 00:19:17 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import { Home } from 'pages/Home';
 import { Movies } from 'pages/Movies';
+import { MovieDetails } from 'pages/MovieDetails';
 import { Routes, Route, NavLink } from 'react-router-dom';
+import { Credits } from './Credits/Credits';
+import { Reviews } from './Reviews/Reviews';
 
 export const App = () => {
   return (
@@ -12,8 +15,12 @@ export const App = () => {
         <NavLink to="/movies">Movies</NavLink>
       </nav>
       <Routes>
-        <Route path="*" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieiId" element={<MovieDetails />}>
+          <Route path="credits" element={<Credits />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
       </Routes>
     </div>
   );
