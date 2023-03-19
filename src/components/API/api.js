41d474acc29db9565fd0ce1ef3f207fa6f4c67be
alexveryk <veryk.av@gmail.com>
2023-03-20 00:19:15 +0200

    Update api.js

--- src/components/API/api.js
+++ src/components/API/api.js
@@ -1,11 +1,37 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/day';
+const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'cc9b731996eb433c4f02d82e82c7e11c';
 
 export const getTrending = async page => {
   const response = await axios.get(
-    `${BASE_URL}?api_key=${API_KEY}&page=${page}`
+    `${BASE_URL}trending/movie/day?api_key=${API_KEY}&page=${page}`
+  );
+  return response;
+};
+
+export const getMovie = async id => {
+  const response = await axios.get(`${BASE_URL}movie/${id}?api_key=${API_KEY}`);
+  return response;
+};
+
+export const getGenres = async () => {
+  const response = await axios.get(
+    `${BASE_URL}genre/movie/list?api_key=${API_KEY}`
+  );
+  return response;
+};
+
+export const getCedits = async id => {
+  const response = await axios.get(
+    `${BASE_URL}movie/${id}/credits?api_key=${API_KEY}`
+  );
+  return response;
+};
+
+export const getReviews = async id => {
+  const response = await axios.get(
+    `${BASE_URL}movie/${id}/reviews?api_key=${API_KEY}&page=1`
   );
   return response;
 };
