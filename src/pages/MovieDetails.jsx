075d90411d05d4cc7f2dfa64ebf88cabd72257d0
alexveryk <veryk.av@gmail.com>
2023-03-22 01:01:34 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,8 +1,10 @@
 import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
 import { useEffect, useRef, useState } from 'react';
 import { getMovie } from 'components/API/api';
+import { MovieWrapper, StyledLink } from './MovieDetails.styled';
+// import { Suspense } from 'react';
 
-export const MovieDetails = () => {
+const MovieDetails = () => {
   const [loading, setLoading] = useState(false);
   const [movie, setMovie] = useState({});
 
@@ -32,28 +34,35 @@ export const MovieDetails = () => {
   return (
     <div>
       {loading && <p>Loading...</p>}
-      <Link to={backLinkref.current}>Go back</Link>
-      <div>
-        <img
-          src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-          alt=""
-        />
-        <h1>{`${movie.original_title}`}</h1>
-        <p>{`User score: ${movie.popularity}`}</p>
-        <h2>Overview</h2>
-        <p>{`${movie.overview}`}</p>
-        <h3>Genres</h3>
-        <p></p>
-        <ul>
-          <li>
-            <Link to="credits">credits</Link>
-          </li>
-          <li>
-            <Link to="reviews">reviews</Link>
-          </li>
-        </ul>
-        <Outlet />
-      </div>
+      <StyledLink to={backLinkref.current}>Go back</StyledLink>
+      <MovieWrapper>
+        <div>
+          <img
+            src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+            alt=""
+          />
+        </div>
+        <div>
+          <h1>{`${movie.original_title}`}</h1>
+          <p>{`User score: ${movie.popularity}`}</p>
+          <h2>Overview</h2>
+          <p>{`${movie.overview}`}</p>
+          <h3>Genres</h3>
+          <p></p>
+        </div>
+      </MovieWrapper>
+      <ul>
+        <li>
+          <Link to="credits">credits</Link>
+        </li>
+        <li>
+          <Link to="reviews">reviews</Link>
+        </li>
+      </ul>
+
+      <Outlet />
     </div>
   );
 };
+
+export default MovieDetails;
