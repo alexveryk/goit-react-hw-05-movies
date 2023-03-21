997aa272fc8096aaaa9db0533858149b6c55a44d
alexveryk <veryk.av@gmail.com>
2023-03-22 01:01:26 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,35 @@
-import { Home } from 'pages/Home';
-import { Movies } from 'pages/Movies';
-import { MovieDetails } from 'pages/MovieDetails';
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import { lazy, Suspense } from 'react';
 import { Credits } from './Credits/Credits';
 import { Reviews } from './Reviews/Reviews';
+import { Containet, NavMenu, NavLinkStyled } from './App.styled';
+
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
 
 export const App = () => {
   return (
-    <div>
-      <nav>
-        <NavLink to="/" end>
+    <Containet>
+      <NavMenu>
+        <NavLinkStyled to="/" end>
           Home
-        </NavLink>
-        <NavLink to="/movies">Movies</NavLink>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieiId" element={<MovieDetails />}>
-          <Route path="credits" element={<Credits />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-      </Routes>
-    </div>
+        </NavLinkStyled>
+        <NavLinkStyled to="/movies">Movies</NavLinkStyled>
+      </NavMenu>
+      <Suspense fallback={<div>Loading... </div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />{' '}
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieiId" element={<MovieDetails />}>
+            <Route path="credits" element={<Credits />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Suspense>
+    </Containet>
   );
 };
